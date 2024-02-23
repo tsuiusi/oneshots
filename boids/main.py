e4109d5522a1ef9e73c99856d2a64bfc23f1d698
tsuiusi <ryantsui786@gmail.com>
2024-02-23 18:02:38 +0000

    boids

--- boids/main.py
+++ boids/main.py
@@ -12,29 +12,56 @@ class boid():
 
         # Parameters
         self.radius = 1
+        self.avfactor = 1
+        self.matchfactor = 1
+        self.flock = 25
     
-    def avoidance(self):
-        pass
+def avoidance(a, neighbors): # b is a list?
+    close_x = 0
+    close_y = 0
+    for i in neighbors:
+        close_x += a.x - i.x
+        close_y += a.y - i.y
 
-    def alignment(self, another):
-        v1 = np.sqrt(self.x_vel^2 + self.y_vel^2)
-        v2 = np.sqrt(another.x_vel^2 + self,y_vel^2)
-        
+    a.x_vel += close_x * a.avfactor
+    a.y_vel += close_y * a.avfactor
+
+def alignment():
+     
+
+def fov(self):
+    pass
+
+def centering(self):
+    pass
+
+def euclidean(a, b):
+    # application of handshake problem so it doesn't go O(N^2)
+    x = a.x - b.x
+    y = a.y - b.y
+
+    return np.sqrt(x^2 + y^2)
+
+def apply_rules(a, boids):
+    tot_x = 0
+    tot_y = 0
+    n = 0
+    for i in boids: 
+        if euclidean(a, i) < a.radius:
+            # Alignment 
+            n += 1
+            tot_x += i.x_vel
+            tot_y += i.y_vel
 
 
-    def centering(self):
-        pass
+   
+    # Alignment
+    a.x_vel += tot_x//n * a.matchfactor
+    a.y_vel += tot_y//n * a.matchfactor
+    
 
-    def euclidean(self, another):
-        # application of handshake problem so it doesn't go O(N^2)
-        x = self.x - another.x
-        y = self.y - another.y
 
-        distance = np.sqrt(x^2 + y^2)
 
-        return distance
 
-    def fov(self):
-        pass
     
 
