099f9971a7030dbd870c28858fad2bb4c8e1a89c
tsuiusi <ryantsui786@gmail.com>
2024-02-28 03:05:38 +0000

    putting everything into the class boid

--- boids/main.py
+++ boids/main.py
@@ -19,70 +19,83 @@ class boid():
         self.flock = 25  
 
     def alignment(self, boid):
-        self.x_vel, boid.x_vel += self.alignfactor * np.mean(self.x_vel, boid.x_vel)
-        self.y_vel, boid.y_vel += self.alignfactor * np.mean(self.y_vel, boid.y_vel)
+        self.x_vel+= self.alignfactor * np.mean(self.x_vel, boid.x_vel)
+        boid.x_vel += self.alignfactor * np.mean(self.x_vel, boid.x_vel)
+        self.y_vel += self.alignfactor * np.mean(self.y_vel, boid.y_vel)
+        boid.y_vel += self.alignfactor * np.mean(self.y_vel, boid.y_vel)
 
     def fov(self, boids):
         domain = []
         for i in boids:
-            if euclidean(i, self) <= self.radius:
+            if self.euclidean(i) <= self.radius:
                 domain.append(i)
         return domain
 
     def avoidance(self, i):
         # not exactly because it does have to align, but would sufficient alignment solve avoidance
-        self.x_vel, i.x_vel += self.alignfactor * i.x_vel, self.alignfactor * self.x_vel
-        self.y_vel, i.y_vel += self.alignfactor * i.y_vel, self.alignfactor * self.y_vel
+        self.x_vel += self.alignfactor * i.x_vel
+        i.x_vel += self.alignfactor * self.x_vel
+        self.y_vel += self.alignfactor * i.y_vel
+        i.y_vel += self.alignfactor * self.y_vel
 
     def centering(self, boids):
         xs = []
         ys = []
         for i in boids:
-            if euclidean(self, i) < self.radius:
+            if self.euclidean(i) < self.radius:
                 xs.append(i.x)
                 ys.append(i.y)
 
         center = np.mean(xs), np.mean(ys)
 
         self.x_vel += self.centerfactor * (np.mean(xs) - self.x)
-        self.y_vel += self.centerfactor * (np.mean(ys) - self.y)         
+        self.y_vel += self.centerfactor * (np.mean(ys) - self.y)    
 
-def euclidean(a, b):
-    # application of handshake problem so it doesn't go O(N^2)
-    x = a.x - b.x
-    y = a.y - b.y
+    def rotation(self):        
+        return np.arctan(np.array([self.y_vel / self.x_vel]))
+    
 
-    return np.sqrt(x^2 + y^2)
-
-def apply_rules(a, boids):
-    tot_x = 0
-    tot_y = 0
-    n = 0
-    for i in boids: 
-        # Centering
-        boid.centering(boids)
-        
-        if euclidean(a, i) < a.radius:
-            # Avoidance
-            boid.avoidance(i)
+    def apply_rules(self, boids):
+        tot_x = 0
+        tot_y = 0
+        n = 0
+        for i in boids: 
+            # Centering
+            self.centering(boids)
             
-            # Alignment 
-            boid.alignment(i)
+            if self.euclidean(i) < self.radius:
+                # Avoidance
+                self.avoidance(i)
+                
+                # Alignment 
+                self.alignment(i)
+
+
+    def euclidean(self, b):
+        # application of handshake problem so it doesn't go O(N^2)
+        x = self.x - b.x
+        y = self.y - b.y
+
+        return np.sqrt(x^2 + y^2)
+
+
 
 def main(n):
     boids = [boid() for _ in range(n)]
 
     while True:
         for i in range(n):
-            apply_rules(boids[n], boids[:n, n:]) # i forgot how to do this
+            boids[i].apply_rules(boids[:i] + boids[i+1:])
 
-        if cv2.waitKey(1) = ord('q'):
+        if cv2.waitKey(1) == ord('q'):
             break
 
     cv2.destroyAllWindows()
 
-if __init__ == "__main__":
-    main()
+if "__init__" == "__main__":
+    main(5)
+
+main(5)
 
 
 
