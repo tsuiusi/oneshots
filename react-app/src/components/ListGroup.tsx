2c85040b69f08e52fa927a0ec48d00740cb3b343
tsuiusi <ryantsui786@gmail.com>
2024-03-11 15:43:28 +0000

    conditional and interactivity

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -13,15 +13,25 @@ function ListGroup() {
 	];
 	// items = [];
 
+	const getMessage = () => {
+		return items.length === 0 ? <p>empty</p> : null;
+	}
+
 	return (
 		<>	
 			<h1>List</h1>
-			{ items.length === 0 ? <p>Empty</p> : null}
+			{/* {items.length === 0 && <p>empty</p>} */}
 			<ul className="list-group">
 				{
 					// If we're retrieving items from an API it's usually a class
 					// So we'll list a property (e.g item.id) instead of just item
-					items.map((item) => (<li>{item}</li>))
+					items.map((item, index) => (<li 
+						className="list-group-item" 
+						key={item} 
+						onClick={() => console.log(item, index)}
+					>
+						{item}
+					</li>))
 				}
 			</ul>
 		</>
