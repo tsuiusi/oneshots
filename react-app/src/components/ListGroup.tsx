198cee282ca2514c9e41b0fe846837c995f82a4a
tsuiusi <ryantsui786@gmail.com>
2024-03-11 15:29:06 +0000

    redrawtiming

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -5,17 +5,24 @@
 // a component cannot return more than 1 element (listgroup can only return listgroup and not h1)
 // this can be solved by wrapping the whole thing in a div or fragments
 function ListGroup() {
-	const items = [
+	let items = [
 		'New York',
 		'Hong Kong', 
-		'London'
+		'London',
+		'Pennsylvania'
 	];
+	// items = [];
 
 	return (
 		<>	
 			<h1>List</h1>
+			{ items.length === 0 ? <p>Empty</p> : null}
 			<ul className="list-group">
-							{items.map((item) => (<li>{item}</li>))}
+				{
+					// If we're retrieving items from an API it's usually a class
+					// So we'll list a property (e.g item.id) instead of just item
+					items.map((item) => (<li>{item}</li>))
+				}
 			</ul>
 		</>
 	);
